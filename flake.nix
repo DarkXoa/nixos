53cf1c57007958fa3696adea57e1e43725199fe5
DarkXoa <travistatum@email.com>
2026-01-16 16:10:56 +0100

    Switch to DMS from Noctalia

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # noctalia = {
+    #   url = "github:noctalia-dev/noctalia-shell";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     stylix = {
       url = "github:nix-community/stylix/release-25.11";
